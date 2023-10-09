ce6a51982e9234bb2d617121cad8d8b8d0628e13
bikalpakc <bikalpa061@gmail.com>
2023-10-09 13:15:32 +0545

    Day 7 completed (Form Validation)

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -9,96 +9,127 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   bool LoginButtonPress = false;
   String name = "";
+  final _formkey = GlobalKey<FormState>();
+  moveToHome(BuildContext) async {
+    if (_formkey.currentState!.validate()) {
+      setState(() {
+        LoginButtonPress = true;
+      });
+      await Future.delayed(Duration(seconds: 1));
+      await Navigator.pushNamed(context, MyRoutes.HomeRoute);
+      setState(() {
+        LoginButtonPress = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Material(
         color: Colors.white,
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              SizedBox(
-                height: 25,
-              ),
-              Image.asset(
-                "assets/images/Login_Image.png",
-                fit: BoxFit.cover,
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Text("Welcome $name",
-                  style: TextStyle(
-                    fontSize: 30,
-                    fontWeight: FontWeight.bold,
-                  )),
-              Padding(
-                padding: const EdgeInsets.symmetric(
-                    vertical: 16.0, horizontal: 32.0),
-                child: Column(
-                  children: [
-                    TextFormField(
-                      decoration: InputDecoration(
-                          hintText: "Enter UserName", labelText: "Username"),
-                      onChanged: (value) {
-                        name = value;
-                        setState(() {});
-                      },
-                    ),
-                    TextFormField(
-                      obscureText: true,
-                      decoration: InputDecoration(
-                          hintText: "Enter Password", labelText: "Password"),
-                    ),
-                    SizedBox(
-                      height: 20,
-                    ),
-
-                    InkWell(
-                      onTap: () async {
-                        setState(() {
-                          LoginButtonPress = true;
-                        });
-                        await Future.delayed(Duration(seconds: 1));
-                        Navigator.pushNamed(context, MyRoutes.HomeRoute);
-                      },
-                      child: AnimatedContainer(
-                        duration: Duration(seconds: 1),
-                        width: LoginButtonPress ? 50 : 70,
-                        height: LoginButtonPress ? 50 : 35,
-                        // color: Colors.brown,
-                        alignment: Alignment.center,
-                        child: LoginButtonPress
-                            ? Icon(
-                                Icons.done,
-                                color: Colors.white,
-                              )
-                            : Text("Login",
-                                style: TextStyle(
-                                  fontSize: 15,
-                                  color: Colors.white,
-                                )),
-                        decoration: BoxDecoration(
-                          color: Colors.brown,
-                          // shape: LoginButtonPress
-                          //     ? BoxShape.circle
-                          //     : BoxShape.rectangle,
-                          borderRadius:
-                              BorderRadius.circular(LoginButtonPress ? 50 : 5),
-                        ),
-                      ),
-                    )
-                    // ElevatedButton(
-                    //   child: Text("Login"),
-                    //   style: TextButton.styleFrom(minimumSize: Size(70, 35)),
-                    //   onPressed: () {
-                    //     // print("Login Sucessful");
-                    //     Navigator.pushNamed(context, MyRoutes.HomeRoute);
-                    //   },
-                    // )
-                  ],
+          child: Form(
+            key: _formkey,
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 25,
                 ),
-              )
-            ],
+                Image.asset(
+                  "assets/images/Login_Image.png",
+                  fit: BoxFit.cover,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text("Welcome $name",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                    )),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                      vertical: 16.0, horizontal: 32.0),
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        decoration: InputDecoration(
+                            hintText: "Enter UserName", labelText: "Username"),
+                        validator: (value) {
+                          if (value != null && value.isEmpty) {
+                            return ("Username cannot be empty");
+                          }
+                          return (null);
+                        },
+                        onChanged: (value) {
+                          name = value;
+                          setState(() {});
+                        },
+                      ),
+                      TextFormField(
+                        obscureText: true,
+                        decoration: InputDecoration(
+                            hintText: "Enter Password", labelText: "Password"),
+                        validator: (value) {
+                          if (value != null && value.isEmpty) {
+                            return ("Password cannot be empty");
+                          } else if (value != null && value.length < 8) {
+                            return ("Password must be atleast 8 characters long");
+                          }
+                          return (null);
+                        },
+                      ),
+
+                      SizedBox(
+                        height: 20,
+                      ),
+
+                      Material(
+                        color: Colors.brown,
+                        borderRadius:
+                            BorderRadius.circular(LoginButtonPress ? 50 : 5),
+                        child: InkWell(
+                          onTap: () => moveToHome(context),
+                          child: AnimatedContainer(
+                            duration: Duration(seconds: 1),
+                            width: LoginButtonPress ? 50 : 70,
+                            height: LoginButtonPress ? 50 : 35,
+                            // color: Colors.brown,
+                            alignment: Alignment.center,
+                            child: LoginButtonPress
+                                ? Icon(
+                                    Icons.done,
+                                    color: Colors.white,
+                                  )
+                                : Text("Login",
+                                    style: TextStyle(
+                                      fontSize: 15,
+                                      color: Colors.white,
+                                    )),
+                            // decoration: BoxDecoration(
+                            //   color: Colors.brown,
+                            //   // shape: LoginButtonPress
+                            //   //     ? BoxShape.circle
+                            //   //     : BoxShape.rectangle,
+                            //   borderRadius:
+                            //       BorderRadius.circular(LoginButtonPress ? 50 : 5),
+                            // ), Comment: We will not be able to use ripple effects on material ancestor if we use docation in this segment. Instead, we use decoration properties in ancestor of this child i.e.Material(above)
+                          ),
+                        ),
+                      )
+                      // ElevatedButton(
+                      //   child: Text("Login"),
+                      //   style: TextButton.styleFrom(minimumSize: Size(70, 35)),
+                      //   onPressed: () {
+                      //     // print("Login Sucessful");
+                      //     Navigator.pushNamed(context, MyRoutes.HomeRoute);
+                      //   },
+                      // )
+                    ],
+                  ),
+                )
+              ],
+            ),
           ),
         ));
   }
